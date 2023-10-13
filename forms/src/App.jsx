1dc13dc1a4763e3a59289a7d5cb01759ebb887f4
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2023-10-13 15:53:55 +0300

    auth forms

---
+++ forms/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Signup from './components/Signup'
+import './index.css'
+
+
+const App = () => {
+  return (
+    
+    <div>
+      <Signup/>
+    </div>
+        
+      
+    
+  )
+}
+
+export default App
